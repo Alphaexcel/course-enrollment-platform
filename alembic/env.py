3161da6d78783770354a5587ea7ba2502ba65cbd
Alphaexcel <ojigbareoyinemi2@gmail.com>
2026-06-07 00:02:00 -0700

    Fix start commnd for Render deployment

--- alembic/env.py
+++ alembic/env.py
@@ -46,7 +46,8 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = os.environ.get("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
+    config.set_main_option("sqlalchemy.url", url)
     context.configure(
         url=url,
         target_metadata=target_metadata,
